0bace9e82043a516c4029da87a99a610274e3f7e
Nicole Ewert <nicole.ewert22@gmail.com>
2016-10-23 18:35:33 -0600

    Added documentation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
+// Prints out the traditional "Hello World!" text
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!"); //print statement
 	}
 }
